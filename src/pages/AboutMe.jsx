df64c0126bfd4516ef029680bcd5bf6e73e36b51
Hadassa Medeiros <122113447+haudacia@users.noreply.github.com>
2025-03-06 17:13:58 +0100

    add tailwind

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 function AboutMe() {
     return (
-        <div>AboutMe</div>
+        <div className='flex m-12 p-8 flex-col w-1/2'>
+            <h1>ABOUT ME</h1>
+            <p>
+                "Hi, I'm Hadassa—an architect and urban planner transitioning into tech. My passion for design, aesthetics, and process optimization led me to full-stack development, where I combine creativity with functionality.
+                I specialize in the MERN stack (MongoDB, Express, React, Node.js), building sleek interfaces and seamless user experiences. I love crafting well-thought-out systems, automating workflows, and ensuring every detail serves a purpose.
+
+                Beyond tech, I'm deeply into art, fashion, and organization. I draw inspiration from the retro-futuristic aesthetics of the 80s, 90s, and 2000s, always exploring new ways to blend visuals with technology.
+
+                If you believe that design and code can transform experiences, let's connect!"
+            </p>
+        </div>
     )
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,63 @@
+const { LuShapes } = require('react-icons/lu')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      borderRadius: {
+        '4xl': '3rem',
+      },
+      backgroundImage: {
+        // 'dotted_pattern': 'url(./assets/grainy.svg)',
+        // 'c1-2': '#ff0000',
+        // 'c1': 'linear-gradient(45deg, rgb(194, 183, 200) 100%, rgb(194, 183, 200) 100%)',
+        'blurry': 'url(/blurry.svg)'
+        // 'c1': 'linear-gradient(45deg, rgba(168,165,200,1) 0%, rgba(192,192,230,1) 43%, rgb(227, 230, 227) 100%)',
+      },
+      backgroundSize: {
+        'dotted_pattern': '300px 300px',
+        'blurry': 'cover'
+      },
+      backgroundRepeat: {
+        'dotted_pattern': 'repeat',
+      },
+      backgroundPosition: {
+        'dotted_pattern': 'center',
+      },
+      colors: {
+        c1: 'rgb(214, 201, 221)',
+        c2: 'rgb(204, 239, 254)',
+        'base-color': 'rgb(234, 207, 207)',
+        'highlight-1': 'rgb(0, 84, 28)',
+        'highlight-2': 'rgb(0, 255, 255)',
+        'dark-bg': 'rgb(255, 20, 147)'
+      },
+      fontFamily: {
+        'rubik': ['Rubik', 'sans-serif'],
+        'space-mono': ['Space Mono', 'monospace'],
+        'abril-fatface': ['Abril Fatface', 'serif']
+      },
+      boxShadow: {
+        'glow': '0 0 10px 5px rgba(255, 255, 255, 0.6)',
+        'vaporwave': '5px 5px 6px rgba(255, 0, 255, 0.6), -5px -5px 6px rgba(0, 255, 255, 0.6)',
+        'crisp': '-5px 5px 0px rgba(0, 0, 0, 1)'
+      },
+      spacing: {
+        'base': '2rem',
+        'double-base': '4rem',
+        'triple-base': '6rem'
+      },
+      screens: {
+        'lg': '1300px',
+      },
+    },
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
+
