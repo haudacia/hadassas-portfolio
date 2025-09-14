73c63cbcb127482bbf0744f8689c3f37ebde0fff
Hadassa Medeiros <122113447+haudacia@users.noreply.github.com>
2025-09-15 01:48:35 +0200

    Actualizar Layout.jsx

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,10 +1,8 @@
 import { Link, Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar";
 
 function Layout() {
   return (
     <div className="container">
-      <Navbar />
       <Outlet />
     </div>
   );
